2afa8720eb39d0834f7f2f80c2a107730ecf58b2
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-03-19 15:43:19 +0200

    Adaugare interfata App first Activity

--- app/src/main/java/com/example/myminiodrive/SigninActivity.java
+++ app/src/main/java/com/example/myminiodrive/SigninActivity.java
@@ -2,7 +2,11 @@ package com.example.myminiodrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        Button btnSignIn=findViewById(R.id.AppSignInButton);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username=findViewById(R.id.editTextTextUserName);
+                EditText password=findViewById(R.id.editTextTextPassword);
+
+                if (username.getText().toString().equals("nisu") && password.getText().toString().equals("parola1123")){
+                    //intent=new Intent(getApplicationContext(), SigninActivity.class);
+                    //startActivity(intent);
+                }
+                else {
+
+                }
+
+            }
+        });
     }
 }
