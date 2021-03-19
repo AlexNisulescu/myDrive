d4f0ea49af267e8d0380e1f3a0071026413508ef
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-03-19 15:26:57 +0200

    Adaugare interfata register

--- app/src/main/java/com/example/myminiodrive/MainActivity.java
+++ app/src/main/java/com/example/myminiodrive/MainActivity.java
@@ -17,13 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnSignIn=findViewById(R.id.signinButton);
+        Button btnRegister=findViewById(R.id.registerButton);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //intent=new Intent(getApplicationContext(), AddActivity.class);
-                //startActivityForResult(intent, REQUEST_CODE);
                 intent=new Intent(getApplicationContext(), SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
         });
