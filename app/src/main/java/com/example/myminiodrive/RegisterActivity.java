11869a7267fad2d7bde250991cce1fc6a7b68553
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-03-24 17:40:59 +0200

    Update inregistrare

--- app/src/main/java/com/example/myminiodrive/RegisterActivity.java
+++ app/src/main/java/com/example/myminiodrive/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.myminiodrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +36,8 @@ public class RegisterActivity extends AppCompatActivity {
                     db.getUsersDao().insert(newUser);
                     Toast.makeText(getApplicationContext(), "You were " +
                             "successfully registered", Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(getApplicationContext(), DriveFirstActivity.class);
+                    startActivity(intent);
                 }
             }
         });
