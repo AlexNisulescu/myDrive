0c3fb2769894b69c4668215a554f6d9388b175c3
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-04-02 21:52:17 +0300

    A lot o new things

--- app/src/main/java/com/example/myminiodrive/DriveSecondActivity.java
+++ app/src/main/java/com/example/myminiodrive/DriveSecondActivity.java
@@ -9,20 +9,32 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.minio.DownloadObjectArgs;
+import io.minio.GetObjectArgs;
 import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.RemoveBucketArgs;
@@ -41,6 +53,9 @@ public class DriveSecondActivity extends AppCompatActivity {
     private MinioClient minioClient;
     private List<Objects> objectList= new ArrayList<>();
     private ListView listView;
+    private InputStream in;
+    private ByteArrayOutputStream out;
+    private byte[] response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +67,7 @@ public class DriveSecondActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        FloatingActionButton btn=findViewById(R.id.floatingActionButton2);
+        FloatingActionButton btn = findViewById(R.id.floatingActionButton2);
 
         Intent intent=getIntent();
 
@@ -70,8 +85,71 @@ public class DriveSecondActivity extends AppCompatActivity {
             loadList();
         }
 
+        /***
+         * This is the code used in order to download an object
+         */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Objects b1=objectList.get(position);
+                AlertDialog dialog=new AlertDialog.Builder(DriveSecondActivity.this).setTitle("Confirmati descarcarea")
+                        .setMessage("Doriti descarcarea")
+                        .setNegativeButton("NU", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInt, int which) {
+                                Toast.makeText(getApplicationContext(), "Datele nu au fost descarcate", Toast.LENGTH_LONG).show();
+                                dialogInt.cancel();
+                            }
+                        }).setPositiveButton("DA", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInt, int which) {
 
+                                try {
+                                    in = new BufferedInputStream(minioClient
+                                            .getObject(GetObjectArgs.builder()
+                                                    .bucket(name).object(b1
+                                                            .getName()).
+                                                            build()));
+                                    out = new ByteArrayOutputStream();
+                                    try {
+                                        byte[] buffer = new byte[2048];
+                                        int n = 0;
+                                        while (-1 != (n = in.read(buffer))) {
+                                            out.write(buffer, 0, n);
+                                            }
+                                        out.close();
+                                        in.close();
+                                        response = out.toByteArray();
+                                        FileOutputStream fos = new FileOutputStream("/sdcard/Download/"+b1.getName());
+                                        fos.write(response);
+                                        fos.close();
+                                    }
+                                    catch(IOException e){
+                                        e.printStackTrace();
+                                    }
 
+                                } catch (ErrorResponseException |
+                                        InsufficientDataException |
+                                        InternalException | InvalidKeyException |
+                                        InvalidResponseException | IOException |
+                                        NoSuchAlgorithmException |
+                                        ServerException |
+                                        XmlParserException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Toast.makeText(getApplicationContext(), "Am descarcat: "+b1.getName(), Toast.LENGTH_LONG).show();
+                                dialogInt.cancel();
+                            }
+                        }).create();
+
+                dialog.show();
+            }
+        });
+
+        /***
+         * This is the code used to delete an object
+         */
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -116,8 +194,58 @@ public class DriveSecondActivity extends AppCompatActivity {
             }
         });
 
+        /***
+         * This is where the search bar magic happens
+         */
+        EditText myTextBox= findViewById(R.id.editTextSearchBar);
+
+        myTextBox.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty()){
+                    try {
+                        objectList.clear();
+                        objectsGetter(name);
+                        loadList();
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                Iterable<Result<Item>> results;
+                results = minioClient.listObjects(
+                        ListObjectsArgs.builder().bucket(name).recursive(true).prefix((s.toString())).build());
+
+                for(Result<Item> res : results){
+                    try {
+                        objectList.clear();
+                        ObjectCustomAdapter adapter = (ObjectCustomAdapter) listView.getAdapter();
+                        Item item = res.get();
+                        Objects obj=new Objects(item.objectName(), item.size(), item.lastModified());
+                        objectList.add(obj);
+                        adapter.notifyDataSetChanged();
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
     }
 
+    /***
+     * This loads the list of objects
+     */
     public void loadList(){
         ObjectCustomAdapter adapter=new ObjectCustomAdapter(getApplicationContext(), R.layout.object_list_view, objectList, getLayoutInflater()){
             @NonNull
@@ -131,6 +259,19 @@ public class DriveSecondActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
     }
 
+    /***
+     * This gets all the objects from the server
+     * @param bucketName
+     * @throws IOException
+     * @throws InvalidKeyException
+     * @throws InvalidResponseException
+     * @throws InsufficientDataException
+     * @throws NoSuchAlgorithmException
+     * @throws ServerException
+     * @throws InternalException
+     * @throws XmlParserException
+     * @throws ErrorResponseException
+     */
     public void objectsGetter(String bucketName) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, ErrorResponseException {
         Iterable<Result<Item>> results;
         minioClient =
