97a285532c4f761640a12d11de3d724adac46d5b
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-04-04 15:41:37 +0300

    Added dark mode functionality

--- app/src/main/java/com/example/myminiodrive/MainActivity.java
+++ app/src/main/java/com/example/myminiodrive/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.myminiodrive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+    public static final String SWITCH_STATE = "switchstate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Switch sw=findViewById(R.id.colorModeSwitch);
         Button btnSignIn=findViewById(R.id.signinButton);
         Button btnRegister=findViewById(R.id.registerButton);
 
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 intent=new Intent(getApplicationContext(), SigninActivity.class);
+                intent.putExtra(SWITCH_STATE, sw.isChecked());
                 startActivity(intent);
             }
         });
@@ -31,8 +37,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 intent=new Intent(getApplicationContext(), RegisterActivity.class);
+                intent.putExtra(SWITCH_STATE, sw.isChecked());
                 startActivity(intent);
             }
         });
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myminiodrive/RegisterActivity.java
+++ app/src/main/java/com/example/myminiodrive/RegisterActivity.java
@@ -1,12 +1,15 @@
 package com.example.myminiodrive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -15,7 +18,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        Intent intent=getIntent();
         Button btnRegister=findViewById(R.id.registerButton2);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +42,19 @@ public class RegisterActivity extends AppCompatActivity {
                     Intent intent=new Intent(getApplicationContext(), DriveFirstActivity.class);
                     startActivity(intent);
                 }
+            }
+        });
+        Switch sw=findViewById(R.id.colorModeSwitch);
+        boolean state=(boolean) intent.getSerializableExtra(MainActivity.SWITCH_STATE);
+        sw.setChecked(state);
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         });
     }
